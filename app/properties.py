fa75ec9443bde6f83b4f7a4995f5f69ab12d6c8f
Mike Sterry <msterry@spscommerce.com>
2020-01-22 15:43:20 -0600

    only 50 threads

--- app/properties.py
+++ app/properties.py
@@ -7,7 +7,7 @@ county_id_lookup_url = 'http://maps1.dnr.state.mn.us/cgi-bin/gazetteer2.cgi?type
 lake_id_lookup_url = 'http://maps2.dnr.state.mn.us/cgi-bin/lakefinder_json.cgi?context=desktop&name=&county='
 lake_info_url = 'http://maps2.dnr.state.mn.us/cgi-bin/lakefinder/detail.cgi?type=lake_survey&id='
 
-number_of_threads = 150
+number_of_threads = 50
 
 csv_output_directory = '/output'
 output_filename = (f'mn_dnr_fish_counts_{current_timestamp}.csv')
